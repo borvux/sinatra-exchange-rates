ed1980880b9a281cfa20e9521eb99455aa594015
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-02-29 00:22:03 +0000

    app looks like the target

--- app.rb
+++ app.rb
@@ -1,9 +1,54 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
+# build the API url, including the API key in the query string
+api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGERATES_KEY"]}"
+
+# use HTTP.get to retrieve the API information
+raw_data = HTTP.get(api_url)
+
+# convert the raw request to a string
+raw_data_string = raw_data.to_s
+
+# convert the string to JSON
+parsed_data = JSON.parse(raw_data_string)
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  #get the currencies from the JSON
+  @currencies_data = parsed_data.fetch("currencies")
+
+  erb(:homepage)
+end
+
+get("/:from_currency") do
+  @currencies_data = parsed_data.fetch("currencies")
+  @original_currency = params.fetch("from_currency").to_s
+
+  erb(:from_currency)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  # build the API url, including the API key in the query string
+  api_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGERATES_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # use HTTP.get to retrieve the API information
+  @raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  @raw_data_string = @raw_data.to_s
+
+  # convert the string to JSON
+  @parsed_data = JSON.parse(@raw_data_string)
+
+  # get the result
+  @result_data = @parsed_data.fetch("result")
+
+
+
+  erb(:from_currency_to_currency)
 end
